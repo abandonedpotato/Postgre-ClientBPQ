86caaf1e719bae1193d93ecd511bee76314c6889
Mr P <theabandonedpotato@gmail.com>
2025-05-22 19:00:49 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import fs from "fs";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: { outDir: "dist" },
+  // This hook copies _redirects to /dist after build
+  closeBundle: () => {
+    const src = resolve(__dirname, "public/_redirects");
+    const dest = resolve(__dirname, "dist/_redirects");
+    if (fs.existsSync(src)) fs.copyFileSync(src, dest);
+  }
+});
